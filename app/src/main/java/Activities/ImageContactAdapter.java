56cee9c53c2eb437ced14c6636d2fe1db7741d58
danilocalderonolivares <dcalderono@ucenfotec.ac.cr>
2020-04-10 21:02:37 -0600

    funcionalidad de buscar ccompleta

--- app/src/main/java/Activities/ImageContactAdapter.java
+++ app/src/main/java/Activities/ImageContactAdapter.java
@@ -20,58 +20,17 @@ import java.util.List;
 
 import Models.POJOS.User;
 
-public class ImageContactAdapter extends RecyclerView.Adapter<ImageContactAdapter.MyViewHolder> {
-    List<User> userList =  new ArrayList<>();
-    Context context;
+public class ImageContactAdapter extends RecyclerView.ViewHolder {
+    TextView fullName;
+    TextView cellPhone;
+    ImageView imageView;
+    ProgressBar progressBar;
+    public ImageContactAdapter(@NonNull View itemView) {
+        super(itemView);
+        fullName = itemView.findViewById(R.id.fullName_userContact);
+        cellPhone = itemView.findViewById(R.id.cellPhone_userContact);
+        imageView = itemView.findViewById(R.id.user_contact_image);
+        progressBar = itemView.findViewById(R.id.loadContacImage);
 
-    public ImageContactAdapter(List<User> puserList, Context pcontext) {
-        this.userList =puserList;
-        this.context = pcontext;
-    }
-
-    @NonNull
-    @Override
-    public MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        LayoutInflater inflater =  LayoutInflater.from(context);
-         View view = inflater.inflate(R.layout.user_contact,parent, false);
-        return new MyViewHolder(view);
-    }
-
-    @Override
-    public void onBindViewHolder(@NonNull MyViewHolder holder, int position) {
-        final User user =  userList.get(position);
-        holder.fullName.setText(" "+user.getName() + " " + user.getLastName());
-        holder.cellPhone.setText(" "+user.getCellPhone());
-
-        if (user.getProfileImage() != null) {
-            Picasso.get()
-                    .load(user.getProfileImage())
-                    .resize(300,300)
-                    .into(holder.imageView);
-            holder.progressBar.setVisibility(View.INVISIBLE);
-        }
-        else {
-            holder.progressBar.setVisibility(View.INVISIBLE);
-            holder.imageView.setImageDrawable(ContextCompat.getDrawable(context,R.drawable.ic_person));
-        }
-
-    }
-
-    @Override
-    public int getItemCount() {
-        return userList.size();
-    }
-    public class MyViewHolder extends RecyclerView.ViewHolder {
-        TextView fullName;
-        TextView cellPhone;
-        ImageView imageView;
-        ProgressBar progressBar;
-        public MyViewHolder(@NonNull View itemView) {
-            super(itemView);
-            fullName = itemView.findViewById(R.id.fullName_userContact);
-            cellPhone = itemView.findViewById(R.id.cellPhone_userContact);
-            imageView = itemView.findViewById(R.id.user_contact_image);
-            progressBar = itemView.findViewById(R.id.loadContacImage);
-        }
     }
 }
--- app/src/main/java/Activities/SearchUser.java
+++ app/src/main/java/Activities/SearchUser.java
@@ -3,33 +3,43 @@ package Activities;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 import androidx.core.view.MenuItemCompat;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.net.Uri;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.SearchView;
 
 import com.example.r24app.R;
+import com.firebase.ui.database.FirebaseListOptions;
+import com.firebase.ui.database.FirebaseRecyclerAdapter;
+import com.firebase.ui.database.FirebaseRecyclerOptions;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
 import com.google.firebase.database.ValueEventListener;
 import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.StorageReference;
+import com.squareup.picasso.Picasso;
 
 import java.io.File;
 import java.io.IOException;
 import java.net.URI;
 import java.util.ArrayList;
 
+import Activities.ReportDetail.ImageAdapter;
 import Models.Constants.FirebaseClasses;
 import Models.POJOS.User;
 
@@ -39,8 +49,9 @@ public class SearchUser extends AppCompatActivity {
     private FirebaseStorage firebaseStorage;
     private StorageReference storageReference;
     private StorageReference ref;
+    FirebaseRecyclerOptions<User> options;
+    FirebaseRecyclerAdapter<User, ImageContactAdapter> adapter;
     ArrayList<User> userList = new ArrayList<>();
-    ArrayList<Uri>  uriArrayList= new ArrayList<>();
     User user =  new User();
     RecyclerView recyclerView;
 
@@ -52,26 +63,11 @@ public class SearchUser extends AppCompatActivity {
         actionBar.setTitle("Lista de usuarios  ");
         database = FirebaseDatabase.getInstance();
         recyclerView = findViewById(R.id.userList);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        recyclerView.setHasFixedSize(true);
         databaseReference = database.getReference(FirebaseClasses.User);
-        databaseReference.addValueEventListener(new ValueEventListener() {
-            @Override
-            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
-                if (dataSnapshot.exists()) {
+        loadDataIntoRecycleView("");
 
-                    for (DataSnapshot snapshot: dataSnapshot.getChildren()) {
-                        user = snapshot.getValue(User.class);
-                        userList.add(user);
-                    }
-                    callRecycleView();
-                }
-
-            }
-
-            @Override
-            public void onCancelled(@NonNull DatabaseError databaseError) {
-
-            }
-        });
     }
 
     @Override
@@ -82,27 +78,58 @@ public class SearchUser extends AppCompatActivity {
         searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
             @Override
             public boolean onQueryTextSubmit(String query) {
-                searchUser(query);
+//                searchUser(query);
+                loadDataIntoRecycleView(query);
                 return false;
             }
 
             @Override
             public boolean onQueryTextChange(String newText) {
-                searchUser(newText);
+//                searchUser(newText);
+                loadDataIntoRecycleView(newText);
                 return false;
             }
         });
         return super.onCreateOptionsMenu(menu);
 
     }
+    private void loadDataIntoRecycleView(String searchText) {
+        Query query = databaseReference.orderByChild("name").startAt(searchText).endAt(searchText + "\uf8ff");
+        options = new FirebaseRecyclerOptions.Builder<User>().setQuery(query,User.class).build();
+        adapter =  new FirebaseRecyclerAdapter<User, ImageContactAdapter>(options) {
+            @Override
+            protected void onBindViewHolder(@NonNull ImageContactAdapter holder, int position, @NonNull User model) {
+                holder.fullName.setText(" "+model.getName() + " " + model.getLastName());
+                holder.cellPhone.setText(" "+model.getCellPhone());
+                if (!model.getProfileImage().isEmpty()) {
+                    Picasso.get()
+                            .load(model.getProfileImage())
+                            .resize(300,300)
+                            .into(holder.imageView);
+                    holder.progressBar.setVisibility(View.INVISIBLE);
 
-    private void callRecycleView() {
+                }else {
+                    holder.progressBar.setVisibility(View.INVISIBLE);
 
-    ImageContactAdapter imageContactAdapter = new ImageContactAdapter(userList, this);
-    recyclerView.setAdapter(imageContactAdapter);
-    recyclerView.setLayoutManager(new LinearLayoutManager(this));
+                    Picasso.get()
+                            .load("https://firebasestorage.googleapis.com/v0/b/r24app-e1e7d.appspot.com/o/myImages%2FemptyImage.jpg?alt=media&token=c216dd47-5de0-46ed-9ef1-79f573d25014")
+                            .resize(300,300)
+                            .into(holder.imageView);
+                }
+            }
+
+            @NonNull
+            @Override
+            public ImageContactAdapter onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+                View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.user_contact,parent, false);
+                return new ImageContactAdapter(view);
+            }
+        };
+        adapter.startListening();
+        recyclerView.setAdapter(adapter);
+    }
+
+
 }
-private void searchUser(String searchText) {
-        System.out.println(searchText);
-}
-}
+
+
