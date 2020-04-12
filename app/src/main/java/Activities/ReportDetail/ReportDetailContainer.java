bf4caeb3686b3bca10beb12d01b28417173972e6
betosr20 <betog19@gmail.com>
2020-04-12 12:02:53 -0600

    Se agrega nuevamente el logo transparente

--- app/src/main/java/Activities/ReportDetail/ReportDetailContainer.java
+++ app/src/main/java/Activities/ReportDetail/ReportDetailContainer.java
@@ -22,6 +22,7 @@ import com.google.firebase.database.ValueEventListener;
 import java.util.ArrayList;
 import java.util.List;
 
+import Activities.MapActivity;
 import Models.Constants.FirebaseClasses;
 import Models.POJOS.ReportPicture;
 
@@ -90,6 +91,8 @@ public class ReportDetailContainer extends AppCompatActivity {
     }
 
     public void windowBack(View v) {
-        onBackPressed();
+        Intent intent = new Intent(this, MapActivity.class);
+        startActivity(intent);
+        finish();
     }
 }
